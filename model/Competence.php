8d1d2f037ad32f8dee8f5ea69962c5a053a3354d
JaegerDaFBoi <92832390+JaegerDaFBoi@users.noreply.github.com>
2022-05-09 13:52:40 -0500

    Update Competence.php

--- model/Competence.php
+++ model/Competence.php
@@ -9,9 +9,45 @@ class Competence extends Model
   public $weekhours;
   public $program;
   
-  public function __construct()
+  public function __construct($code, $description, $weekhours, $program)
   {
     parent::__construct();
+    $this->code = $code;
+    $this->description = $description;
+    $this->weekhours = $weekhours;
+    $this->program = $program;
+  }
+  
+  public function saveCompetence()
+  {
+    try
+  {
+    $sql = "INSERT INTO competencias (codigo,descripcion,horassemana,fk_programa) VALUES (:codigo,:descripcion,:horassemana,:programa)";
+    $query = $this->connection->prepare($sql);
+    $query->execute([":codigo"=>$this->code, ":descripcion"=>$this->description,":horassemana"=>$this->weekhours,":programa"=>$this->program]);
+  }
+    catch(PDOException $e)
+  {
+    die("Error al guardar datos: ".$e->getMessage());
+  }
   }
 
+  public function showTable()
+  {
+    try
+  {
+    $sql = $this->connection->query("SELECT id, codigo, descripcion, horassemana FROM competencias");
+    $competences = $sql->fetchAll(PDO::FETCH_OBJ);
+    return $competences;
+  }
+  catch(PDOException $e)
+  {
+    die("Error al traer datos: ".$e->getMessage());
+  }
+  }
+
+  public function searchCompetence()
+  {
+
+  }
 }
